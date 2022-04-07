4e04f2ec867857bc9aa48f40ebe425384bcc0f7b
augustas.cirba <augustas.cirba@geniussports.com>
2022-04-07 12:15:53 +0300

    Error handling for missing output icon

--- custom_components/eldes_alarm/switch.py
+++ custom_components/eldes_alarm/switch.py
@@ -61,7 +61,11 @@ class EldesSwitch(EldesDeviceEntity, SwitchEntity):
     @property
     def icon(self):
         """Return the icon of this sensor."""
-        return OUTPUT_ICONS_MAP[self.data["outputs"][self.entity_index].get("iconName", "ICON_1")]
+        try:
+            return OUTPUT_ICONS_MAP[self.data["outputs"][self.entity_index]["iconName"]]
+        except ValueError:
+            _LOGGER.error("Unknown output icon for (%s)", self.data['outputs'][self.entity_index]['name'])
+            return OUTPUT_ICONS_MAP["ICON_1"]
 
     async def async_turn_on(self):
         """Turn the entity on."""
